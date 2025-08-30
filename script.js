66681900b3b7d5d9558f9e161ef7a74b39c9fbae
ravi-108 <ravi.10819011922@ipu.ac.in>
2025-08-30 18:05:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const API_KEY = "0b81afc2bf76495192cff2185e14891a";
+const API_URL = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+async function fetchNews(query) {
+  try {
+    const response = await fetch(`${API_URL}${query}&apiKey=${API_KEY}`);
+    const data = await response.json();
+    displayNews(data.articles);
+  } catch (error) {
+    console.error("Error fetching news:", error);
+  }
+}
+
+function displayNews(articles) {
+  const template = document.getElementById("template-news-card");
+  const newsContainer = document.getElementById("news-container");
+
+  newsContainer.innerHTML = ""; // clear previous results
+
+  articles.forEach((article) => {
+    if (!article.urlToImage) return; // skip if no image
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("img").src = article.urlToImage;
+    clone.querySelector("img").alt = article.title;
+    clone.querySelector("h2").textContent = article.title;
+    clone.querySelector("p").textContent =
+      article.description || "No description available";
+    clone.firstElementChild.addEventListener("click", () => {
+      window.open(article.url, "_blank");
+    });
+    newsContainer.appendChild(clone);
+  });
+
+
+
+}
+
+const searchNews = (query) => {
+  const newsContainer = document.getElementById("news-container");
+  if(!query) return;
+  newsContainer.innerHTML = ""; // clear previous results
+  fetchNews(query);
+}
